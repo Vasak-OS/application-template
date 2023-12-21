268ecf3780392b0fc17f34b977f2a6438891526c
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-21 09:40:28 -0300

    Add frameless window functionality to AplicationWindow class

--- src/AplicationWindow.py
+++ src/AplicationWindow.py
@@ -1,6 +1,7 @@
 from Vasak.VSKWindow import VSKWindow
 from src.AplicationBinding import AplicationBinding
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt
 
 class AplicationWindow(VSKWindow):
     def __init__(self, app: QApplication):
@@ -8,3 +9,8 @@ class AplicationWindow(VSKWindow):
         self.shareObject = AplicationBinding(self, app)
         self.channel.registerObject("vsk", self.shareObject)
         self.load_html("ui/dist/index.html") # Cargar un HTML en el WebView
+
+    def set_position(self):
+        self.setWindowFlags(
+            self.windowFlags() | Qt.WindowType.FramelessWindowHint 
+        )
