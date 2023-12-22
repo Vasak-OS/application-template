71bb477258c0a4a3a86a293725a1123cab149c74
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-22 14:34:26 -0300

    Add application metadata

--- application-template.py
+++ application-template.py
@@ -7,6 +7,9 @@ from PyQt6.QtWidgets import QApplication
 app = QApplication(sys.argv)
 
 if __name__ == "__main__":
+    app.setApplicationName("Application Template")
+    app.setApplicationVersion("0.0.1")
+    app.setOrganizationName("Vasak Group")
     window = AplicationWindow(app)
     window.show()
     signal.signal(signal.SIGINT, signal.SIG_DFL)  # Habilitar Ctrl+C
