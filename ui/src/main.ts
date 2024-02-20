ff3d668c23ca441b7af44eba0cc4caf5eae85cf9
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-02-20 11:20:52 -0300

    fix: Actualizar la inyección de dependencia de $vsk en main.ts para composition api

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -6,7 +6,7 @@ import App from './App.vue';
 
 const startApp = (channel: any) => {
   const app = createApp(App);
-  app.config.globalProperties.$vsk = channel.objects.vsk;
+  app.provide('vsk', channel.objects.vsk);
   app.use(createPinia());
 
   app.mount('#app');
