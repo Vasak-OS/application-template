7c54ccc7070008a7dcfbbdffef1f1f0522acc657
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-05-23 11:32:10 -0300

    chore: Eliminar dependencia de vue-i18n y actualizar configuración de idiomas

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,13 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import i18n from './plugins/i18n';
 import App from './App.vue';
 
 const startApp = (channel: any) => {
   const app = createApp(App);
   app.provide('vsk', channel.objects.vsk);
   app.use(createPinia());
-  app.use(i18n());
 
   app.mount('#app');
 };
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,16 +2,10 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 export default defineConfig({
   base: '',
-  plugins: [
-    vue(),
-    VueI18nPlugin({
-      include: [fileURLToPath(new URL('./src/locales/**', import.meta.url))]
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
